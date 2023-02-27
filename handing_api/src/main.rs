d9335d3496718ad468f75056bdb975448a2fa2fd
Pallab Kalita <13886153+pallab@users.noreply.github.com>
2023-02-27 10:17:57 +0530

    async request

--- handing_api/src/main.rs
+++ handing_api/src/main.rs
@@ -9,7 +9,25 @@ error_chain!{
     }
 }
 
-fn main() -> Result<()> {
+fn main()  {
+    blocking_request();
+    async_request();
+}
+
+#[tokio::main]
+async fn async_request() -> Result<()> {
+    let result = reqwest::get("http://httpbin.org/get").await?;
+
+    println!("Status: {}", result.status());
+    println!("Headers : \n{:#?}", result.headers());
+
+    let body = result.text().await?;
+    println!("Body : \n{}", body);
+    Ok(())
+}
+
+
+fn blocking_request() -> Result<()> {
     let mut result = reqwest::blocking::get("http://httpbin.org/get")?;
     let mut body = String::new();
     result.read_to_string(&mut body)?;
