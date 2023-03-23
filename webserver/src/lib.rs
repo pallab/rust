2c7602a7eeda508685939e89471edebfbc58aa66
Pallab Kalita <13886153+pallab@users.noreply.github.com>
2023-03-23 14:45:55 +0530

    multi threading

---
+++ webserver/src/lib.rs
@@ -0,0 +1,67 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::thread::JoinHandle;
+
+type  Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+
+    /// Creates a new worker with thread on standby
+    ///
+    /// `id` is the id of worker
+    ///
+    /// # Panics
+    ///
+    /// The `new` functions panics if there aren't enough system
+    /// resources to create a new thread
+    fn new(id: usize, receiver : Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = std::thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} received a job");
+            job()
+        });
+        Worker { id, thread }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender : mpsc::Sender<Job>
+}
+
+impl ThreadPool {
+    /// Creates a new thread pool with the given number of threads
+    ///
+    /// `size` is number of threads
+    ///
+    /// # Panics
+    ///
+    /// The `new` function panics if size is less than 1
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        // a thread safe receiver
+        // Arc will let threads share the receiver while keeping thrack of ownership
+        // mutex will ensure only one thread can mutate the receiver at any one point in time
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers: Vec<Worker> = Vec::with_capacity(size);
+        for i in 0..size {
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static
+    {
+        self.sender.send(Box::new(f)).unwrap();
+    }
+}
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -2,17 +2,21 @@ use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::net::Shutdown::Read;
+use std::time::Duration;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let thread_pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
-        handle_stream(stream);
+        println!("New Connection");
+        thread_pool.execute(|| handle_stream(stream));
     }
 
-    println!("Hello, world!");
+    println!("Shutting Down");
 }
 
 fn handle_stream(mut stream : TcpStream) {
@@ -26,6 +30,10 @@ fn handle_stream(mut stream : TcpStream) {
 
     let (status, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /slow HTTP/1.1" => {
+            std::thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        },
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
